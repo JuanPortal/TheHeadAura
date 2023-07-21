325667de2723e9e89037b59e66488104a1e3116e
Portal <juanportalg@gmail.com>
2023-07-21 12:46:03 -0700

    project started

---
+++ src/HeadAura.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './main.css'
+
+export const HeadAura = () => {
+  return (
+    <h1>Head Aura</h1>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HeadAura } from './HeadAura'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HeadAura />
+  </React.StrictMode>,
+)
